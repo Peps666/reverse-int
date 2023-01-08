cb5e9a9a6745667cd1df9e748795b027f1ce7ba3
Peps666 <yauhenkursy@gmail.com>
2023-01-08 13:11:13 +0300

    add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
-}
+    let result = 0;
+    let modul = Math.abs(n);
+    while ( modul > 0) {
+        result = result * 10 + modul % 10;
+        modul = Math.floor ( modul / 10);
+    };
+    return result;
+};
